6a87134082f689876f190e26efd74f3a401d8399
Abhijeet Padhy <abhijeet.padhy@gmail.com>
2022-05-29 16:08:46 +0530

    spring-boot-starter-security

---
+++ src/main/java/com/example/demo/web/WebController.java
@@ -0,0 +1,23 @@
+package com.example.demo.web;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/secretpage")
+    public String display(Model model){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        model.addAttribute("username", username);
+        return "secret";
+    }
+}
